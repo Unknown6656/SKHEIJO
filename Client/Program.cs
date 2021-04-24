91769af5e7e545328a211c4d2987e965d3507c59
unknown6656 <unknown84@gmx.de>
2021-04-24 18:09:28 +0200

    added test programs

---
+++ Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System;
+
+using SKHEIJO;
+
+
+try
+{
+    string? s;
+
+    do
+        s = Console.ReadLine();
+    while (s is null);
+
+    using GameClient client = new(s);
+
+    Console.WriteLine("press ENTER to exit");
+    Console.ReadLine();
+
+    client.Dispose();
+}
+catch (Exception? ex)
+{
+    StringBuilder sb = new();
+
+    while (ex is Exception)
+    {
+        sb.Insert(0, $"[{ex.GetType()}] {ex.Message}:\n{ex.StackTrace}\n");
+
+        ex = ex.InnerException;
+    }
+
+    Console.WriteLine(sb);
+
+    if (Debugger.IsAttached)
+    {
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(true);
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using SKHEIJO;
+
+namespace Server
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Game game = new(new Player[] { new(1), new(2), new(3) });
+            game.ResetAndDealCards(150, 0);
+
+            Console.WriteLine(game.CurrentPlayer.ToString());
+
+
+
+            // using GameServer server = await GameServer.CreateGameServer(14488);
+            using GameServer server = GameServer.CreateLocalGameServer("127.0.0.1", 14488);
+
+            Console.WriteLine(server.ConnectionString);
+            Console.WriteLine("\npress ESC to exit.");
+
+            server.Start();
+
+            while (server.IsRunning)
+                if (Console.KeyAvailable && Console.ReadKey(true).Key is ConsoleKey.Escape)
+                    server.Stop();
+        }
+    }
+}
