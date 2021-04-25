77515d4557eae8da19512594e2ec91fc44a01b7f
unknown6656 <unknown84@gmx.de>
2021-04-25 23:43:13 +0200

    added game window

---
+++ Client/GameWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+
+namespace SKHEIJO
+{
+    public sealed partial class GameWindow
+        : Window
+    {
+        public GameClient GameClient { get; }
+
+
+        public GameWindow(GameClient game)
+        {
+            InitializeComponent();
+
+            GameClient = game;
+        }
+    }
+}
