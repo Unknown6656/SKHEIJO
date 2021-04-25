23e6bbf6171e0a61f49361249232e8fd3f3329c2
unknown6656 <unknown84@gmx.de>
2021-04-25 23:19:54 +0200

    improved the logging API

--- Server/Logger.cs
+++ Server/Logger.cs
@@ -2,34 +2,71 @@
 using System.Threading.Tasks;
 using System;
 
+using Fleck;
+
 namespace SKHEIJO
 {
     public enum LogSeverity
     {
         Debug,
-        OK,
+        Info,
         Warning,
         Error,
     }
 
+    public enum LogSource
+    {
+        Unknown,
+        Log,
+        Server,
+        Client,
+        WebServer,
+        UI,
+        Game,
+    }
+
     public static class Logger
     {
-        private static readonly ConcurrentQueue<(DateTime time, LogSeverity severity, string msg)> _queue = new();
+        private static readonly ConcurrentQueue<(DateTime time, LogSource source, LogSeverity severity, string msg)> _queue = new();
         private static readonly ConsoleColor _initial_color = ConsoleColor.Gray;
         private static readonly object _mutex = new();
+        private static LogSeverity _level = LogSeverity.Debug;
 
-        public static LogSeverity MinimumSeverityLevel { get; set; } = LogSeverity.Debug;
+
         public static bool IsRunning { get; private set; }
 
+        public static LogSeverity MinimumSeverityLevel
+        {
+            get => _level;
+            set
+            {
+                _level = value;
 
-        static Logger() => _initial_color = Console.ForegroundColor;
+                FleckLog.Level = value switch
+                {
+                    LogSeverity.Info => LogLevel.Info,
+                    LogSeverity.Warning => LogLevel.Warn,
+                    LogSeverity.Error => LogLevel.Error,
+                    _ => LogLevel.Debug,
+                };
+            }
+        }
+
+
+        static Logger()
+        {
+            _initial_color = Console.ForegroundColor;
+            MinimumSeverityLevel = LogSeverity.Debug;
+
+            FleckLog.LogAction = LogWebsocket;
+        }
 
         public static void Start() => Task.Factory.StartNew(async delegate
         {
             if (IsRunning)
                 return;
 
-            Log("Logger has been started.");
+            "Logger has been started.".Log(LogSource.Log);
 
             IsRunning = true;
 
@@ -41,6 +78,19 @@ namespace SKHEIJO
 
             Console.ForegroundColor = _initial_color;
         });
+
+        public static async Task Stop()
+        {
+            if (!IsRunning)
+                return;
+
+            "Logger has been stopped.".Log(LogSource.Log);
+
+            IsRunning = false;
+
+            while (!_queue.IsEmpty)
+                await Task.Delay(1);
+        }
 
         private static bool Flush()
         {
@@ -55,7 +105,7 @@ namespace SKHEIJO
                     {
                         (ConsoleColor color, string prefix) = item.severity switch
                         {
-                            LogSeverity.OK => (ConsoleColor.Green, " OK "),
+                            LogSeverity.Info => (ConsoleColor.Cyan, "INFO"),
                             LogSeverity.Warning => (ConsoleColor.Yellow, "WARN"),
                             LogSeverity.Error => (ConsoleColor.Red, "ERR."),
                             _ => (ConsoleColor.White, "    "),
@@ -70,7 +120,12 @@ namespace SKHEIJO
                         Console.ForegroundColor = color;
                         Console.Write(prefix);
                         Console.ForegroundColor = ConsoleColor.DarkGray;
-                        Console.Write("]  ");
+                        Console.Write("][");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(item.source);
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write(']');
+                        Console.CursorLeft = 36;
                         Console.ForegroundColor = color;
                         Console.WriteLine(item.msg);
                     }
@@ -79,27 +134,31 @@ namespace SKHEIJO
             return any;
         }
 
-        public static async Task Stop()
+        public static void Warn(this object? obj, LogSource source) => Log(obj, source, LogSeverity.Warning);
+
+        public static void Err(this object? obj, LogSource source) => Log(obj, source, LogSeverity.Error);
+
+        public static void Info(this object? obj, LogSource source) => Log(obj, source, LogSeverity.Info);
+
+        public static void Log(this object? obj, LogSource source) => Log(obj, source, LogSeverity.Debug);
+
+        public static void Log(this object? obj, LogSource source, LogSeverity severity) =>
+            _queue.Enqueue((DateTime.Now, source, severity, obj as string ?? obj?.ToString() ?? ""));
+
+        private static void LogWebsocket(LogLevel level, string message, Exception? ex)
         {
-            if (!IsRunning)
-                return;
+            LogSeverity severity = level switch
+            {
+                LogLevel.Info => LogSeverity.Info,
+                LogLevel.Warn => LogSeverity.Warning,
+                LogLevel.Error => LogSeverity.Error,
+                _ => LogSeverity.Debug,
+            };
 
-            Log("Logger has been stopped.");
+            Log(message, LogSource.WebServer, severity);
 
-            IsRunning = false;
-
-            while (!_queue.IsEmpty)
-                await Task.Delay(1);
+            if (ex is { })
+                Log(ex, LogSource.WebServer, severity);
         }
-
-        public static void Warn(this object? obj) => Log(obj, LogSeverity.Warning);
-
-        public static void Err(this object? obj) => Log(obj, LogSeverity.Error);
-
-        public static void Ok(this object? obj) => Log(obj, LogSeverity.OK);
-
-        public static void Log(this object? obj) => Log(obj, LogSeverity.Debug);
-
-        public static void Log(this object? obj, LogSeverity severity) => _queue.Enqueue((DateTime.Now, severity, obj as string ?? obj?.ToString() ?? ""));
     }
 }
