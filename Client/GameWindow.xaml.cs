e84136bedffc749beddcd1399a40a0e3037ea1ad
unknown6656 <unknown84@gmx.de>
2021-04-25 13:37:48 +0200

    added the main window

---
+++ Client/GameWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SKHEIJO
+{
+    public sealed partial class GameWindow
+        : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        //string? s;
+        //
+        //do
+        //    s = Console.ReadLine();
+        //while (s is null);
+        //
+        //using GameClient client = new(Guid.NewGuid(), s);
+        //
+        //Console.WriteLine("type 'q' to exit");
+        //
+        //while (true)
+        //{
+        //    string line = Console.ReadLine() ?? "";
+        //
+        //    if (line == "q")
+        //        break;
+        //    else
+        //        From.Bytes(await client.SendMessageAndWaitForReply(From.String(line))).ToString().Log();
+        //}
+        //
+        //client.Dispose();
+
+
+
+
+    }
+}
