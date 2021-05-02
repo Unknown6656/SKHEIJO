58eeaeae82b8a0c48a71a916c78783132b642441
unknown6656 <unknown84@gmx.de>
2021-05-03 01:31:06 +0200

    reworked the UUID class

--- Server/web/js/guid.js
+++ Server/web/js/guid.js
@@ -1,33 +1,66 @@
+const __UUID_REGEX__ = /^\s*\{?\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b\}?\s*$/gi;
 
-function UUID(bytes)
+class UUID
 {
-    this.bytes = bytes === undefined ? new Uint8Array(16) : bytes;
-}
-
-UUID.prototype.toString = function()
-{
-    var str = '{';
-
-    for (var i = 0; i < this.bytes.length; ++i)
+    constructor(bytes)
     {
-        str += ('0' + (this.bytes[i] & 0xFF).toString(16)).slice(-2);
-
-        if (i == 3 || i == 5 || i == 7 || i == 9)
-            str += '-';
+        this.bytes = bytes == undefined ? new Uint8Array(16) : bytes;
     }
 
-    return str + '}';
+    toString(include_curlies = false)
+    {
+        var str = include_curlies ? '{' : '';
+
+        for (var i = 0; i < this.bytes.length; ++i)
+        {
+            str += ('0' + (this.bytes[i] & 0xFF).toString(16)).slice(-2);
+
+            if (i == 3 || i == 5 || i == 7 || i == 9)
+                str += '-';
+        }
+
+        if (include_curlies)
+            str += '}';
+
+        return str;
+    }
+
+    equals(obj)
+    {
+        return obj instanceof UUID && toString() == obj.toString();
+    }
 }
 
-let EmptyUUID = () => new UUID();
+UUID.Empty = new UUID();
 
-let NewUUID = function()
+UUID.New = () =>
 {
-    let uuid = new UUID();
+    let bytes = new Uint8Array(16);
 
-    crypto.getRandomValues(uuid.bytes);
-    uuid.bytes[6] = (uuid.bytes[6] & 0x0f) | 0x40;
-    uuid.bytes[8] = (uuid.bytes[8] & 0x3f) | 0x80;
+    for (var i = 0; i < bytes.length; ++i)
+    {
+        bytes[i] = Math.floor(Math.random() * 256);
 
-    return uuid;
-}
+        if (i == 6)
+            bytes[i] = (bytes[i] & 0x0f) | 0x40;
+        else if (i == 8)
+            bytes[i] = (bytes[i] & 0x3f) | 0x80;
+    }
+
+    return new UUID(bytes);
+};
+
+UUID.Parse = string =>
+{
+    let match = string.match(__UUID_REGEX__);
+
+    if (match.length)
+    {
+        let bytes = new Uint8Array(match[0].match(/[0-9a-f]{2}/g).map(b => parseInt(b, 16)));
+
+        return new UUID(bytes);
+    }
+    else
+        return null;
+};
+
